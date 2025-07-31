a1a80bb059850baca0bf50452c6b32995a3333d1
frontdevtool <kazem84@hotmail.com>
2025-07-31 08:35:21 +0100

    sing-in-up screens

--- app/(auth)/_layout.jsx
+++ app/(auth)/_layout.jsx
@@ -1,20 +1,21 @@
-import { View, Text, StatusBar } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
+import { View, Text, StatusBar } from "react-native";
+import React from "react";
+import { Stack } from "expo-router";
 
 const AuthLayout = () => {
   return (
     <>
-    <Stack screenOptions={{
-      headerShown : false
-    }}>
-      <Stack.Screen name='sign-in'  />
-      <Stack.Screen name='sign-up'  />
-    </Stack>
-        <StatusBar barStyle={""} backgroundColor={"#161622"} />
-  
-      </>
-  )
-}
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="sign-in" />
+        <Stack.Screen name="sign-up" />
+      </Stack>
+      <StatusBar barStyle={""} backgroundColor={"#161622"} />
+    </>
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
