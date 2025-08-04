8b2632bc34fea71bcfd03c135b83d04fcc3d14e8
frontdevtool <kazem84@hotmail.com>
2025-08-04 13:15:18 +0100

    react-hook-form + zod

--- app/(auth)/Sign-in.jsx
+++ app/(auth)/Sign-in.jsx
@@ -8,40 +8,46 @@ import CustomButton from "@/components/CustomButton";
 import { Link, router } from "expo-router";
 
 const SignIn = () => {
-const [form, setForm] = useState({email : '' , password : ''})
-const [text, setText] = useState('')
-return (
-<SafeAreaView className="flex-1 bg-primary p-2">
-    <ScrollView
-    //  className=' p-1' 
-    
-    //  style={{minHeight : 450}}
-      // contentContainerStyle={{height : 600}}
-     >
+  const [form, setForm] = useState({ email: "", password: "" });
+  const [text, setText] = useState("");
+  return (
+    <SafeAreaView className="flex-1 bg-primary p-2">
+      <ScrollView>
         <View className="w-full p-4 justify-center  h-full">
-            <Image source={images.logo} className="w-[130px] h-[84px] " resizeMode="contain" />
-            <Text className="text-white text-2xl font-pbold my-8">Sign-in</Text>
-            <FormField
-            placeholder={'email'}
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px] "
+            resizeMode="contain"
+          />
+          <Text className="text-white text-2xl font-pbold my-8">Sign-in</Text>
+          <FormField
+            placeholder={"email"}
             keyboardType={"email-address"}
             value={form.email}
-             onChangeText={(e)=>setForm({...form , email : e})}
-             />
-            <FormField
-            otherstyle={'my-7'}
-            // label={password}
-             keyboardType={"number-pad"}
-              placeholder={'password'}
+            onChangeText={(e) => setForm({ ...form, email: e })}
+          />
+          <FormField
+            otherstyle={"my-7"}
+            keyboardType={"number-pad"}
+            placeholder={"password"}
             value={form.password}
-             onChangeText={(e)=>setForm({...form , password : e})}
-             />
-        <CustomButton title={'log-in'} handlePress={()=>router.push('/home')}/>
-          <Link className=" text-center mt-4 text-white" href={'/(auth)/sign-up'}>Don't have account ? <Text className="text-secondary-100">sign-up</Text></Link>
-             
+            onChangeText={(e) => setForm({ ...form, password: e })}
+          />
+          <CustomButton
+            title={"log-in"}
+            handlePress={() => router.push("/home")}
+          />
+          <Link
+            className=" text-center mt-4 text-white"
+            href={"/(auth)/sign-up"}
+          >
+            Don't have account ?{" "}
+            <Text className="text-secondary-100">sign-up</Text>
+          </Link>
         </View>
-    </ScrollView>
-</SafeAreaView>
-)
+      </ScrollView>
+    </SafeAreaView>
+  );
 };
 
 export default SignIn;
